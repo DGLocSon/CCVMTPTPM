67c5dcfc2c7f9fc0df09251ca266b515878bdf11
DGLocSon <Omonguyen0@gmail.com>
2026-02-05 22:57:34 +0700

    feat: Update Login feature

--- src/main/java/com/example/demo/login.java
+++ src/main/java/com/example/demo/login.java
@@ -1,1 +1,15 @@
-login.java
+package com.example.demo;
+
+class login {
+
+    public String login() {
+    // Simulate a login process
+    String username = "admin";
+    String password = "password";
+    if (username == "admin" && password == "password") {
+        return "Login successful!";
+    } else {
+        return "Invalid username or password.";
+    }
+}
+}
